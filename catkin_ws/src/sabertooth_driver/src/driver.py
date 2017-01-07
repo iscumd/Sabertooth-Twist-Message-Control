2b09243bcf946fe8e228af99c0c3f52c06803ee4
airfield20 <afcofiel@umich.edu>
2017-01-07 17:12:16 -0500

    updated driver.py

--- catkin_ws/src/sabertooth_driver/src/driver.py
+++ catkin_ws/src/sabertooth_driver/src/driver.py
@@ -6,8 +6,11 @@ import serial
 import numpy as np
 import tf.transformations
 from geometry_msgs.msg import Twist
-
-address = 128
+#reverse: address 128 motor 2 and address 129 motor 2
+address1 = 128
+address2 = 129
+#WHEEL_BASE = 37
+#WHEEL_RADIUS = 15
 WHEEL_BASE = 10
 WHEEL_RADIUS = 2
 #function to scale values in one range to values in another range -  proportional scaling
@@ -17,27 +20,28 @@ def pscale(x, in_min, in_max, out_min, out_max):
 def motor1speed(address,speed):
     port = serial.Serial('/dev/ttyUSB0')
     port.isOpen()
-    command = 6
-    checksum = (address + command + speed) & 127
-    print("M1 Speed: " + str(speed))
-    port.write("".join(chr(i) for i in [address, 14, 50, (address + 64) & 127]))
-    port.write("".join(chr(i) for i in [address, command, speed, checksum]))
+    command1 = 6
+    command2 = 7
+    checksum1 = (address + command1 + speed) & 127
+    checksum2 = (address + command2 + pscale(speed,0,127,127,0)) & 127
+    print("Left Speed: " + str(speed))
+    port.write("".join(chr(i) for i in [address, 14, 10, (address + 24) & 127]))
+    port.write("".join(chr(i) for i in [address, command1, speed, checksum1]))
+    port.write("".join(chr(i) for i in [address, command2, pscale(speed,0,127,127,0), checksum2]))
 
 def motor2speed(address,speed):
     port = serial.Serial('/dev/ttyUSB0')
     port.isOpen()
-    command = 7
-    checksum = (address + command + speed) & 127
-    print("M2 Speed: " + str(speed))
-    port.write("".join(chr(i) for i in [address, 14, 50, (address + 64) & 127]))
-    port.write("".join(chr(i) for i in [address, command, speed, checksum]))
+    command1 = 6
+    command2 = 7
+    checksum1 = (address + command1 + speed) & 127
+    checksum2 = (address + command2 + pscale(speed,0,127,127,0)) & 127
+    print("Right Speed: " + str(speed))
+    port.write("".join(chr(i) for i in [address, 14, 10, (address + 24) & 127]))
+    port.write("".join(chr(i) for i in [address, command1, speed, checksum1]))
+    port.write("".join(chr(i) for i in [address, command2, pscale(speed,0,127,127,0), checksum2]))
 
 def callback(msg):
-    linear_velocity_arr = np.array([msg.linear.x, msg.linear.y, msg.linear.z])
-    angular_velocity_arr = np.array([msg.angular.x, msg.angular.y, msg.angular.z])
-
-    linear_velocity = np.linalg.norm(linear_velocity_arr)
-    angular_velocity = np.linalg.norm(angular_velocity_arr)
     print("twist received")
     rospy.loginfo("Twist Message")
     rospy.loginfo("Linear Components: [%f, %f, %f]"%(msg.linear.x, msg.linear.y, msg.linear.z))
@@ -51,14 +55,14 @@ def callback(msg):
     # to map the values from (-2.8,2.8) to (0,127)
     scaledv1 = pscale(v1,-1.5,1.5,0,127)
     scaledv2 = pscale(v2,-1.5,1.5,0,127)
-    motor1speed(address,int(round(scaledv1,2)))
-    motor2speed(address,int(round(scaledv2,2)))
+    motor1speed(address1,int(round(scaledv1,2)))
+    motor2speed(address2,int(round(scaledv2,2)))
 
 def listener():
 
     rospy.init_node('sabertooth_driver') #anonymous=true would allow us to run multiple of these motor interpreters at once
 
-    rospy.Subscriber("cmd_vel", Twist, callback)
+    rospy.Subscriber("cmd_vel", Twist, callback,queue_size = 2)
 
     rospy.spin() #keeps python from exiting until node is stopped
 
